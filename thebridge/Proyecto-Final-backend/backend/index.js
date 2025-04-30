5b32d85c1870fb1aea06676cdfb89c68df2b1a64
unknown <adriaponsfebrer@gmail.com>
2025-04-30 18:30:25 +0200

    fix: netlify origen 2

--- thebridge/Proyecto-Final-backend/backend/index.js
+++ thebridge/Proyecto-Final-backend/backend/index.js
@@ -6,8 +6,12 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-  origin: "https://lively-cannoli-0689ff.netlify.app"
+  origin: "https://lively-cannoli-0689ff.netlify.app",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
